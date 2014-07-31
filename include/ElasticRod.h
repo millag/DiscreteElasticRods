5c1d8d8427e23ec612484322631b831963e1dde4
millag <millag43@gmail.com>
2014-07-31 18:11:09 +0100

    compute material curvature for each edge

--- include/ElasticRod.h
+++ include/ElasticRod.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include "ngl/Vec4.h"
+#include "ngl/Vec2.h"
 
 class ElasticRod {
 
@@ -12,7 +13,7 @@ public:
     ~ElasticRod();
 
 /// calculations are carried out in the coordinate space of pos
-/// so user must assure everything is specified in the same coordinate space
+/// must assure everything is in same coordinate space
     void init(const std::vector<ngl::Vec4>& restpos,
               const ngl::Vec4 u0,
               const std::vector<ngl::Vec4>& pos,
@@ -24,19 +25,21 @@ public:
 
     void update(ngl::Real dt);
 
-///    bending stiffness B = [ EI1 0  ]   =  [ materialAlpha  0             ]
-///                          [ 0   EI2]      [ 0              material alpha]
+///    bending stiffness B = [EI1 0  ]   =  [bendStiffnes  0           ]
+///                          [0   EI2]      [0             bendStiffnes]
 ///    depends on the Young modulus E(this is material property measured and constant throughout simulation)
-///    and the area moment of inertia I1 or I2 which in turn depend only on cross-section
+///    and the area moment of inertia I1 or I2 which in turn depend only on cross-section area
 ///    note that I1 = I2 when the cross-section is circular, thus we always assume circular cross-section here
-///    otherwise there should be 2 values describing the stiffness in each direction
-///    FIX: need to calculate alpha by using the thickness and density
+///    otherwise there should be 2 values describing the stiffness in each material axis direction
+///    FIX: need to calculate bendStiffnes by using the thickness and density
     ngl::Real m_bendStiffnes;
 ///    twisting stiffness m_twistStiffness = GJ
-//    depends on the shear modulus G = E/(2(1 + v)) where v is the Poison ratio (this is material property measured and constant throughout simulation)
-//    and the area moment of twist J which in turn depends only on cross-section
-//    FIX: need to calculate alpha by using the thickness and density
+///    depends on the shear modulus G = E/(2(1 + v)) where E is Young modulus and v is the Poison ratio
+///    (this are material properties measured and constant throughout simulation)
+///    and the area moment of twist J which in turn depends only on cross-section area
+///    FIX: need to calculate twistStiffness by using the thickness and density
     ngl::Real m_twistStiffness;
+
     unsigned m_nIterations;
 
 //    ========= should be private ========
@@ -49,25 +52,38 @@ public:
     std::vector<ngl::Real> m_pmass;
 /// #isFixed = n + 1
     std::vector<bool> m_pIsFixed;
-/// should be unit length and perpendicular to the first edge
+/// unit length vector - defines the Bishop frame of the first edge
     ngl::Vec4 m_u0;
-/// #twistAngle = #edges = n
-    std::vector<ngl::Real> m_twistAngle;
+
 
 
 private:
-/// #edges = #vertices - 1
+/// #twistAngle = #edges = n
+    std::vector<ngl::Real> m_twistAngle;
+/// #restWprev = #edges - restWprev[i] defines rest material curvatures at e[i -1]
+    std::vector<ngl::Vec2> m_restWprev;
+/// #restWnext = #edges - restWnext[i] defines rest material curvatures at e[i]
+    std::vector<ngl::Vec2> m_restWnext;
+/// #restEdgeL = #edges = n edge length
+    std::vector<ngl::Real> m_restEdgeL;
+/// #restEdgeL = #edges = n
+/// NOTE: there is no restRegionL[0] => restRegionL[0] = 0
+    std::vector<ngl::Real> m_restRegionL;
+
+/// #edges = #vertices - 1 defines tangent axis between v[i + 1] and v[i]
     std::vector<ngl::Vec4> m_edges;
-/// defined at vertices 1 ... n - 1 i.e. #kb = (#edges - 1)
-/// #kb = #edges - defines rotation from e[i-1] to e[i], kb[0] = (0, 0, 0, 0);
-    std::vector<ngl::Vec4> m_kb;
 /// #m1 = #edges - m1[i] defines material axis m1 at e[i]
     std::vector<ngl::Vec4> m_m1;
 /// #m2 = #edges - m2[i] defines material axis m2 at e[i]
     std::vector<ngl::Vec4> m_m2;
+/// defined at vertices 1 ... n - 1 i.e. #kb = (#edges - 1)
+/// #kb = #edges - defines rotation from e[i-1] to e[i],
+/// NOTE: there is no kb[0] => kb[0] = (0, 0, 0, 0);
+    std::vector<ngl::Vec4> m_kb;
 
-    std::vector<ngl::Real> m_restEdgeL;
-    std::vector<ngl::Real> m_restRegionL;
+
+
+
 
     ngl::Real m_totalTwist;
     ngl::Real m_totalL;
@@ -81,6 +97,7 @@ private:
 /// For n + 1 vertices there are n edges
     void computeEdges(const std::vector<ngl::Vec4>& vertices,
                       std::vector<ngl::Vec4>& o_edges) const;
+
 /// Computes rest edge lengths the li scalars:
 /// l[i] = |e[i - 1]| + |e[i + 1]|
 /// NOTE: there is no l_0
@@ -88,18 +105,37 @@ private:
                         std::vector<ngl::Real> &o_edgeL,
                         std::vector<ngl::Real> &o_regionL, ngl::Real &o_totalL) const;
 
-/// Computes the curvature binormal kb 3-vectors
+    void extractSinAndCos(const ngl::Real& magnitude,
+                          ngl::Real& o_sinPhi, ngl::Real& o_cosPhi) const;
+
+/// Computes the curvature binormal kb 3-vectors which depend on m_restEdgeL state
 /// NOTE: there is no kb_0
 /// depend on m_restEdgeL state
     void computeKB(const std::vector<ngl::Vec4>& edges,
                    std::vector<ngl::Vec4>& o_kb) const;
 
+/// Computes Bishop frame for every edge by parallel transporting around the curvature binormal kb
+/// the method internally computes kb 3-vectors
+    void computeBishopFrame(const ngl::Vec4& u0,
+                            const std::vector<ngl::Vec4>& edges,
+                            std::vector<ngl::Vec4>& o_kb,
+                            std::vector<ngl::Vec4>& o_u,
+                            std::vector<ngl::Vec4>& o_v) const;
+
+/// Computes Material frame for every edge - defining the orientation of the edge
+/// the method internally computes kb 3-vectors, Bishop frame and rotates Bishop frame with twist angle
     void computeMaterialFrame(const ngl::Vec4& u0,
                               const std::vector<ngl::Vec4>& edges,
                               const std::vector<ngl::Real>& twistAngle,
                               std::vector<ngl::Vec4>& o_kb,
                               std::vector<ngl::Vec4>& o_m1,
                               std::vector<ngl::Vec4>& o_m2) const;
+
+    void computeMaterialCurvature(const std::vector<ngl::Vec4> &kb,
+                                  const std::vector<ngl::Vec4>& m1,
+                                  const std::vector<ngl::Vec4>& m2,
+                                  std::vector<ngl::Vec2>& o_Wprev,
+                                  std::vector<ngl::Vec2>& o_Wnext) const;
 
     void computeForces(const std::vector<ngl::Vec4>& vertices,
                        std::vector<ngl::Vec4>& o_forces) const;
@@ -111,13 +147,11 @@ private:
                             const std::vector<ngl::Vec4>& edges,
                             const std::vector<ngl::Vec4>& kb,
                             std::vector<ngl::Vec4>& o_twistForces) const;
-    void computeStretchForces(const std::vector<ngl::Vec4>& vertices,
-                                const std::vector<ngl::Vec4>& edges,
-                                std::vector<ngl::Vec4>& o_stretchForces) const;
 
 
     void computeEdgeMatrices(const std::vector<ngl::Vec4>& edges, std::vector<ngl::Mat4>& o_edgeMatrices) const;
     void computeMatrixMult(const ngl::Vec4& kb, const ngl::Vec4& e, ngl::Mat4& o_matrix) const;
+
 
 };
 
--- src/ElasticRod.cpp
+++ src/ElasticRod.cpp
@@ -9,8 +9,6 @@ ElasticRod::ElasticRod()
 {
     m_bendStiffnes = 0.6;
     m_twistStiffness = 1.0;
-
-
 }
 
 ElasticRod::~ElasticRod()
@@ -41,15 +39,14 @@ void ElasticRod::init(const std::vector<ngl::Vec4>& restpos,
     m_pIsFixed = isFixed;
 
     m_totalTwist = 8.;
-//    init edges & lengths
-    computeEdges(m_ppos, m_edges);
-
+//    compute edges & lengths for rest shape
+    computeEdges(restpos, m_edges);
     computeLengths(m_edges, m_restEdgeL, m_restRegionL, m_totalL);
-//    init kb and material frame
-    computeMaterialFrame(m_u0, m_edges, m_twistAngle, m_kb, m_m1, m_m2);
+//    compute kb and material frame for rest shape
+    computeBishopFrame(m_u0, m_edges, m_kb, m_m1, m_m2);
 
-    //TODO: precompute rest wi
-    //compute material frame
+//    precompute material curvature for rest shape
+    computeMaterialCurvature(m_kb, m_m1, m_m2, m_restWprev,  m_restWnext);
 }
 
 
@@ -97,26 +94,81 @@ void ElasticRod::computeKB(const std::vector<ngl::Vec4>& edges,
     o_kb.resize(edges.size());
     o_kb[0].set(0, 0, 0, 0);
 
-    ngl::Real magnitude, tan;
+    ngl::Real magnitude;
     ngl::Real sinPhi, cosPhi;
     for (unsigned i = 1; i < edges.size(); ++i)
     {
-///     NOTE: in the paper they claim the following formula produces |kb| = 2 * tan(phi / 2)
-///     where phi is the angle between the 2 edges
+///     NOTE: as metioned in the paper the following formula produces |kb| = 2 * tan(phi / 2),
+///     where phi is the angle of rotation defined by the 2 edges
         o_kb[i] = 2 * edges[i - 1].cross(edges[i]) /
                 (m_restEdgeL[i - 1] * m_restEdgeL[i] + edges[i - 1].dot(edges[i]));
 
-        assert( fabs(o_kb[i].dot(edges[i - 1])) < utils::ERR );
-        assert( fabs(o_kb[i].dot(edges[i])) < utils::ERR );
 //      need to assert that |kb| = 2 * tan(phi / 2)
         magnitude = o_kb[i].lengthSquared();
-        if (magnitude  > 0.1)
-            cosPhi = 0;
-        cosPhi = sqrt(4.0 / (4.0 + magnitude));
-        sinPhi = sqrt(magnitude / (4.0 + magnitude));
-        tan = 4 * sinPhi * sinPhi / (cosPhi * cosPhi);
-        assert( fabs((sinPhi * sinPhi + cosPhi * cosPhi) - 1) < utils::ERR );
-        assert( fabs(magnitude - tan) < utils::ERR );
+        extractSinAndCos(magnitude, sinPhi, cosPhi);
+        assert( fabs(o_kb[i].dot(edges[i - 1])) < utils::ERR );
+        assert( fabs(o_kb[i].dot(edges[i])) < utils::ERR );
+        assert( fabs(magnitude - 4 * sinPhi * sinPhi / (cosPhi * cosPhi)) < utils::ERR );
+    }
+}
+
+void ElasticRod::extractSinAndCos(const ngl::Real& magnitude,
+                      ngl::Real& o_sinPhi, ngl::Real& o_cosPhi) const
+{
+    o_cosPhi = sqrt(4.0 / (4.0 + magnitude));
+    o_sinPhi = sqrt(magnitude / (4.0 + magnitude));
+
+    assert( fabs((o_sinPhi * o_sinPhi + o_cosPhi * o_cosPhi) - 1) < utils::ERR );
+    assert( fabs(magnitude - 4.0 * o_sinPhi * o_sinPhi / (o_cosPhi * o_cosPhi)) < utils::ERR );
+}
+
+void ElasticRod::computeBishopFrame(const ngl::Vec4& u0,
+                        const std::vector<ngl::Vec4>& edges,
+                        std::vector<ngl::Vec4>& o_kb,
+                        std::vector<ngl::Vec4>& o_u,
+                        std::vector<ngl::Vec4>& o_v) const
+{
+    assert( edges.size() == (m_ppos.size() - 1) );
+//    check if Bishop frame is correctly defined
+    assert( fabs(u0.lengthSquared() - 1) < utils::ERR );
+    assert( fabs(u0.dot(edges[0])) < utils::ERR );
+
+    o_u.resize(edges.size());
+    o_v.resize(edges.size());
+
+    computeKB(edges, o_kb);
+    o_u[0] = u0;
+    o_v[0] = edges[0].cross(o_u[0]);
+    o_v[0].normalize();
+
+    ngl::Real magnitude;
+    ngl::Real sinPhi, cosPhi;
+    ngl::Quaternion q, p;
+//    compute Bishop frame for current configuration given u0 and edges
+    for (unsigned i = 1; i < o_u.size(); ++i)
+    {
+        magnitude = o_kb[i].lengthSquared();
+        if (magnitude < utils::ERR)
+        {
+            o_u[i] = o_u[i - 1];
+            o_v[i] = o_v[i - 1];
+            continue;
+        }
+//        here sinPhi and cosPhi are derived from the length of kb |kb| = 2 * tan( phi/2 )
+        extractSinAndCos(magnitude, sinPhi, cosPhi);
+        magnitude = 1.0 / sqrt(magnitude);
+//        rotate frame u axis around kb
+        q.set(cosPhi, sinPhi * o_kb[i].m_x * magnitude, sinPhi * o_kb[i].m_y * magnitude, sinPhi * o_kb[i].m_z * magnitude);
+        p.set(0, o_u[i - 1].m_x, o_u[i - 1].m_y, o_u[i - 1].m_z);
+        p = (q * p * q.conjugate());
+
+        o_u[i] = p.getVector();
+        o_v[i] = edges[i].cross(o_u[i]);
+        o_v[i].normalize();
+
+        assert( fabs(p.getS()) <  utils::ERR );
+        assert( fabs(o_u[i].dot(edges[i])) < utils::ERR );
+        assert( fabs(o_u[i].lengthSquared() - 1) < utils::ERR );
     }
 }
 
@@ -129,65 +181,45 @@ void ElasticRod::computeMaterialFrame(const ngl::Vec4& u0,
 {
     assert( edges.size() == (m_ppos.size() - 1) );
     assert( twistAngle.size() == edges.size() );
-//    check if Bishop frame is correctly defined
-    assert( fabs(u0.lengthSquared() - 1) < utils::ERR );
-    assert( fabs(u0.dot(edges[0])) < utils::ERR );
 
     o_m1.resize(edges.size());
     o_m2.resize(edges.size());
 
-    computeKB(edges, o_kb);
-    o_m1[0] = u0;
-    o_m2[0] = edges[0].cross(o_m1[0]);
-    o_m2[0].normalize();
+    computeBishopFrame(u0, edges, o_kb, o_m1, o_m2);
 
-    ngl::Real magnitude;
     ngl::Real sinPhi, cosPhi;
-    ngl::Quaternion q, p;
-//    compute Bishop frame for current configuration given u0 and edges
-    for (unsigned i = 1; i < o_m1.size(); ++i)
-    {
-        magnitude = o_kb[i].lengthSquared();
-        if (magnitude < utils::ERR)
-        {
-            o_m1[i] = o_m1[i - 1];
-            o_m2[i] = o_m2[i - 1];
-            continue;
-        }
-//        here sinPhi and cosPhi are derived from the length of kb |kb| = 2 * tan( phi/2 )
-        cosPhi = sqrt(4.0 / (4.0 + magnitude));
-        sinPhi = sqrt(magnitude / (4.0 + magnitude));
-        assert( fabs(magnitude - 4 * sinPhi * sinPhi / (cosPhi * cosPhi)) < utils::ERR );
-
-        magnitude = 1.0 / sqrt(magnitude);
-        q.set(cosPhi, sinPhi * o_kb[i].m_x * magnitude, sinPhi * o_kb[i].m_y * magnitude, sinPhi * o_kb[i].m_z * magnitude);
-        p.set(0, o_m1[i - 1].m_x, o_m1[i - 1].m_y, o_m1[i - 1].m_z);
-
-        p = (q * p * q.conjugate());
-        o_m1[i] = p.getVector();
-
-        assert( fabs(p.getS()) <  utils::ERR );
-        assert( fabs(o_m1[i].dot(edges[i])) < utils::ERR );
-        assert( fabs(o_m1[i].lengthSquared() - 1) < utils::ERR );
-
-        o_m2[i] = edges[i].cross(o_m1[i]);
-        o_m2[i].normalize();
-    }
-
     ngl::Vec4 m1, m2;
     for (unsigned i = 0; i < o_m1.size(); ++i)
     {
         sinPhi = sin(twistAngle[i]);
         cosPhi = cos(twistAngle[i]);
-
         m1 = cosPhi * o_m1[i] + sinPhi * o_m2[i];
         m2 = -sinPhi * o_m1[i] + cosPhi * o_m2[i];
-        assert( fabs(m1.dot(m2)) < utils::ERR );
-        assert( fabs(m1.dot(edges[i])) < utils::ERR );
-        assert( fabs(m2.dot(edges[i])) < utils::ERR );
 
         o_m1[i] = m1;
         o_m2[i] = m2;
+
+        assert( fabs(o_m1[i].dot(o_m2[i])) < utils::ERR );
+        assert( fabs(o_m1[i].dot(edges[i])) < utils::ERR );
+        assert( fabs(o_m2[i].dot(edges[i])) < utils::ERR );
+    }
+}
+
+void ElasticRod::computeMaterialCurvature(const std::vector<ngl::Vec4>& kb,
+                                        const std::vector<ngl::Vec4>& m1,
+                                        const std::vector<ngl::Vec4>& m2,
+                                        std::vector<ngl::Vec2>& o_Wprev,
+                                        std::vector<ngl::Vec2>& o_Wnext) const
+{
+    o_Wprev.resize(kb.size());
+    o_Wnext.resize(kb.size());
+
+    o_wprev[0].set(0, 0);
+    o_wnext[0].set(0, 0);
+    for (unsigned i = 1; i < kb.size(); ++i)
+    {
+        o_Wprev.set(kb[i].dot(m2[i - 1]), -kb[i].dot(m1[i - 1]));
+        o_Wnext.set(kb[i].dot(m2[i])    , -kb[i].dot(m1[i])    );
     }
 }
 
@@ -392,35 +424,6 @@ void ElasticRod::computeTwistForces(const std::vector<ngl::Vec4>& vertices,
 
     o_twistForces[0] = jminus_term[1];
     o_twistForces[o_twistForces.size() - 1] = jplus_term[o_twistForces.size() - 2];
-}
-
-/**
- * Computes the forces used to enforce the inextensibility constraint.
- **/
-void ElasticRod::computeStretchForces(const std::vector<ngl::Vec4>& vertices,
-                                        const std::vector<ngl::Vec4>& edges,
-                                        std::vector<ngl::Vec4>& o_stretchForces) const
-{
-    o_stretchForces.resize(vertices.size());
-    for (unsigned i = 1; i < edges.size(); ++i)
-    {
-        o_stretchForces[i] = edges[i] * (4.0 / m_restEdgeL[i]) *
-                            (edges[i].dot(edges[i]) - m_restEdgeL[i] * m_restEdgeL[i]);
-        o_stretchForces[i] -= edges[i - 1] * (4.0 / m_restEdgeL[i - 1]) *
-                            (edges[i - 1].dot(edges[i - 1]) - m_restEdgeL[i - 1] * m_restEdgeL[i - 1]);
-    }
-
-    unsigned idx = 0;
-    o_stretchForces[idx] = edges[idx] * (4.0 / m_restEdgeL[idx]) *
-                        (edges[idx].dot(edges[idx]) - m_restEdgeL[idx] * m_restEdgeL[idx]);
-
-    idx = edges.size();
-
-    assert(idx == (o_stretchForces.size() - 1));
-
-    o_stretchForces[idx] = (-1.0) * edges[idx - 1] * (4.0 / m_restEdgeL[idx - 1]) *
-                           (edges[idx - 1].dot(edges[idx - 1]) - m_restEdgeL[idx - 1] * m_restEdgeL[idx - 1]);
-
 }
 
 
