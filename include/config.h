552b63a8ef69de2ec26038ca89f0370d43a62d87
millag <millag43@gmail.com>
2017-07-09 15:11:44 +0300

    fix whitespacing

--- include/config.h
+++ include/config.h
@@ -9,27 +9,25 @@
 
 namespace mg
 {
-    using  namespace cml;
+	using namespace cml;
 
-    typedef float Real;
+	typedef float Real;
 
-    typedef constantsf Constants;
+	typedef constantsf Constants;
 
-    typedef vector2f Vec2D;
-    typedef vector3f Vec3D;
-    typedef vector4f Vec4D;
+	typedef vector2f Vec2D;
+	typedef vector3f Vec3D;
+	typedef vector4f Vec4D;
 
-    typedef matrix22f_c Matrix2D;
-    typedef matrix33f_c Matrix3D;
-    typedef matrix44f_c Matrix4D;
+	typedef matrix22f_c Matrix2D;
+	typedef matrix33f_c Matrix3D;
+	typedef matrix44f_c Matrix4D;
 
-    typedef matrix23f_c Matrix23D;
+	typedef matrix23f_c Matrix23D;
 
-    typedef matrix<Real, dynamic<>, col_basis, col_major> MatrixND;
+	typedef matrix<Real, dynamic<>, col_basis, col_major> MatrixND;
 
-    typedef quaternion<Real, fixed<>, scalar_first> Quaternion;
+	typedef quaternion<Real, fixed<>, scalar_first> Quaternion;
 }
-
-
 
 #endif // CONFIG_H
