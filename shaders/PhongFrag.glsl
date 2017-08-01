f6893980e585ea0c2812c4efd41f390184596be0
millag <millag43@gmail.com>
2017-08-01 12:27:33 +0300

    phong reflection model

--- shaders/PhongFrag.glsl
+++ shaders/PhongFrag.glsl
@@ -2,24 +2,24 @@
 
 struct Light
 {
+	/// light ambient emission color
+	vec3 ambient;
+	/// light diffuse emission color
+	vec3 diffuse;
+	/// light specular emission color
+	vec3 specular;
 	/// light position in view space
 	vec3 position;
-	/// light ambient emission color
-	vec4 ambient;
-	/// light diffuse emission color
-	vec4 diffuse;
-	/// light specular emission color
-	vec4 specular;
 };
 
 struct Material
 {
 	/// ambient reflection color
-	vec4 ambient;
+	vec3 ambient;
 	/// diffuse reflection color
-	vec4 diffuse;
+	vec3 diffuse;
 	/// specular reflection color
-	vec4 specular;
+	vec3 specular;
 	/// shininess - larger for surfaces that are smoother and more mirror-like
 	float shininess;
 };
@@ -41,11 +41,14 @@ layout (location = 0) out vec4 fragColor;
 void main ()
 {
 	// calculations take place in view space
-	vec3 ldir = light.position - fr_pos;
-	vec3 rdir = reflect(ldir, fr_normal);
-	vec3 vdir = -fr_pos;
+	vec3 ldir = normalize(light.position - fr_pos);
+	vec3 ndir = normalize(fr_normal);
+	vec3 rdir = reflect(ldir, ndir);
+	vec3 vdir = -normalize(fr_pos);
 
-	fragColor = mtl.ambient * light.ambient +
-			(mtl.diffuse * dot(ldir, fr_normal) * light.diffuse +
-			 mtl.specular * pow(dot(rdir, vdir), mtl.shininess) * light.specular);
+	vec3 color = light.ambient * mtl.ambient +
+			(light.diffuse * mtl.diffuse * max(dot(ldir, ndir), 0.) +
+			 light.specular * mtl.specular * pow(max(dot(rdir, vdir), 0.), mtl.shininess));
+
+	fragColor = vec4(color, 1.0);
 }
--- shaders/PhongVert.glsl
+++ shaders/PhongVert.glsl
@@ -1,13 +1,9 @@
 #version 410
 
-/// model matrix
-uniform mat4 m;
 /// model view matrix
 uniform mat4 mv;
 /// model view projection matrix
 uniform mat4 mvp;
-/// normal matrix - from model to view space
-uniform mat3 nm;
 
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 normal;
@@ -21,7 +17,7 @@ out vec3 fr_normal;
 void main()
 {
 	fr_pos = (mv * vec4(position, 1.0)).xyz;
-	fr_normal = normalize(nm * normal);
+	fr_normal = (transpose(inverse(mv)) * vec4(normal, 0.0)).xyz;
 
 	gl_Position = mvp * vec4(position, 1.0);
 }
