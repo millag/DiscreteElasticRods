f70936eeeabd7004aaadc32170d239ea811ba6ec
mgrigorova <mgrigorova@viewray.com>
2021-09-17 14:35:26 +0300

    fix parsing error due to Qt locale setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,60 @@
 #include "MainWindow.h"
 #include "Scene.h"
 #include "SceneLoader.h"
+#include "BasicParser.h"
 
 #include <QApplication>
 
-void InitializeGLDefaultSurfaceFormat()
+void InitGLDefaultSurfaceFormat()
 {
-	auto format = QSurfaceFormat::defaultFormat();
-	format.setProfile( QSurfaceFormat::CoreProfile );
-	format.setSwapBehavior( QSurfaceFormat::DoubleBuffer );
-	format.setDepthBufferSize( 24 );
-	format.setStencilBufferSize( 8 );
-	format.setSamples( 4 );
-	QSurfaceFormat::setDefaultFormat( format );
+    auto format = QSurfaceFormat::defaultFormat();
+    format.setProfile( QSurfaceFormat::CoreProfile );
+    format.setSwapBehavior( QSurfaceFormat::DoubleBuffer );
+    format.setDepthBufferSize( 24 );
+    format.setStencilBufferSize( 8 );
+    format.setSamples( 4 );
+    QSurfaceFormat::setDefaultFormat( format );
 }
 
-void test()
-{
-	for ( auto f = 2.0; f <= 3.0; f += 0.05 )
-	{
-		std::cout << " f = " << f
-		          << " (int)(f) = " << static_cast<int>( f )
-		          << " xi = " << static_cast<int>( f + 0.5f )
-		          << " xf = " << f + 0.5f - static_cast<int>( f + 0.5f )
-		          << "\n";
-	}
-	std::cout << std::endl;
+void test() {
+    char szOrbits[] = "365.24 29.53";
+//    char* pEnd;
+    qDebug() << BasicParser::parseDouble(szOrbits);
+//    double d1, d2;
+//    d1 = std::strtod (szOrbits, &pEnd);
+//    d2 = std::strtod (pEnd, NULL);
+//    qDebug("The moon completes %.2f orbits per Earth year.\n", d1/d2);
 
 }
 
 int main(int argc, char *argv[])
 {
-//	test();
-//	return 0;
+//    test();
+//    return 0;
 
-	InitializeGLDefaultSurfaceFormat();
+    qInfo() << "Multi-threading" << ( ( MULTI_THREADING_ON )? "enabled" : "disabled" );
 
-	QApplication app(argc, argv);
+    try
+    {
+        // create our scene
+        Scene scene;
+        SceneLoader loader;
+        // loader.loadTestScene( scene );
+        loader.loadScene( "assets/scene1_long_curly.mg", scene );
+        scene.initialize();
 
-	qDebug() << "Multi-threading" << ( ( MULTI_THREADING_ON )? "enabled" : "disabled" );
+        InitGLDefaultSurfaceFormat();
+        QApplication app(argc, argv);
 
-	try
-	{
-//		create our scene
-		Scene scene;
-		SceneLoader loader;
-//		loader.loadTestScene( scene );
-		loader.loadScene( "assets/scene1_long_curly.mg", scene );
-		scene.initialize();
+        MainWindow window;
+        window.setScene( scene );
+        window.show();
+        return app.exec();
+    }
+    catch( std::exception& e )
+    {
+        qFatal("Fatal error: %s", e.what());
+    }
 
-		MainWindow window;
-		window.setScene( scene );
-		window.show();
-		return app.exec();
-	}
-	catch( std::exception& e )
-	{
-		qInfo() << "Fatal error: "
-		        << e.what();
-	}
-
-	return 0;
+    return 0;
 }
